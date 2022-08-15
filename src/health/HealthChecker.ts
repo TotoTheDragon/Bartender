6d0ff22553cad645ec89d5a92da5aeeb076794ea
MrDragonPlays <thomasdvandeweerd@gmail.com>
2022-08-15 22:24:59 +0200

    Fixed object typing in HealthChecker

--- src/health/HealthChecker.ts
+++ src/health/HealthChecker.ts
@@ -1,7 +1,7 @@
 export default abstract class HealthChecker {
     name: string;
 
-    lastCheck: ({ healthy: boolean } & object) | null;
+    lastCheck: { healthy: boolean; [key: string]: any } | null;
 
     constructor(name: string) {
         this.name = name;
