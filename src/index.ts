eabb0c5e51d9eb6fd32d6d6d91111def99f48165
MrDragonPlays <thomasdvandeweerd@gmail.com>
2022-08-15 21:22:55 +0200

    Added winston as logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
 import { config } from 'dotenv';
 import fastify from 'fastify';
+import winston from 'winston';
 import DatabaseManager from './database/DatabaseManager';
 import HealthManager from './health/HealthManager';
 import BartenderRouter from './router';
 
 config();
-// Load all required things
+/*
+    Set up logging
+*/
+const logger = winston.createLogger({});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+    );
+}
+
+process.on('uncaughtException', (err) => {
+    logger.error('Uncaught error occurred', { err });
+});
+/*
+    Initialize all required things
+*/
+
 const healthManager = new HealthManager();
 const databaseManager = new DatabaseManager({});
 const server = fastify();
+
 // Set up fastify instance
-server.register(BartenderRouter, { prefix: '/api' });
+server.decorate('logger', logger);
 server.decorate('health-manager', healthManager);
 server.decorate('db-manager', databaseManager);
+server.register(BartenderRouter, { prefix: '/api' });
 
 // Set up health manager
 healthManager.registerDependency('Postgres Database', databaseManager);
@@ -24,5 +46,5 @@ healthManager.registerDependency('Postgres Database', databaseManager);
 
 (async () => {
     const address = await server.listen({ port: 3000 });
-    console.log(`Now running api at ${address}`);
+    logger.info('API is now running', address);
 })();
