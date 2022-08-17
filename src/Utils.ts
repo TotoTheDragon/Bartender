73ca2939c1a5acc96ae2ba5da24e1f8683d3563b
Dragon <thomasdvandeweerd@gmail.com>
2022-08-17 16:47:46 +0200

    Added function to fetch the product quantity

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,22 +1,15 @@
+import convert, { Unit } from 'convert-units';
+
 /* eslint-disable no-param-reassign */
 export default class Utils {
-    static mapObjectValue<T, V>(
-        object: { [key: string]: T },
-        mapFn: (_: T) => V,
-    ) {
-        return Object.keys(object).reduce(
-            (result: { [key: string]: V }, key) => {
-                result[key] = mapFn(object[key]);
-                return result;
-            },
-            {},
-        );
+    static mapObjectValue<T, V>(object: { [key: string]: T }, mapFn: (_: T) => V) {
+        return Object.keys(object).reduce((result: { [key: string]: V }, key) => {
+            result[key] = mapFn(object[key]);
+            return result;
+        }, {});
     }
 
-    static fillInDefaults(
-        object: object | undefined,
-        defaults: object,
-    ): object {
+    static fillInDefaults(object: object | undefined, defaults: object): object {
         const obj: { [key: string]: any } = {};
         if (object !== undefined) {
             Object.entries(object).forEach(([key, val]) => {
@@ -35,8 +28,57 @@ export default class Utils {
     }
 
     static average(nums: number[]): number | undefined {
-        return nums.length
-            ? nums.reduce((a, b) => a + b, 0) / nums.length
-            : undefined;
+        return nums.length ? nums.reduce((a, b) => a + b, 0) / nums.length : undefined;
+    }
+
+    static getSmallestMatchingUnit(unit: Unit): Unit {
+        switch (convert().describe(unit).measure) {
+        case 'angle':
+            return 'deg';
+        case 'apparentPower':
+            return 'mVA';
+        case 'area':
+            return 'mm2';
+        case 'current':
+            return 'mA';
+        case 'ditgital':
+            return 'B';
+        case 'energy':
+            return 'mWh';
+        case 'frequency':
+            return 'mHz';
+        case 'illuminance':
+            return 'lx';
+        case 'length':
+            return 'mm';
+        case 'mass':
+            return 'mg';
+        case 'pace':
+            return 's/m';
+        case 'partsPer':
+            return 'ppq';
+        case 'power':
+            return 'mW';
+        case 'pressure':
+            return 'Pa';
+        case 'reactiveEnergy':
+            return 'mVARh';
+        case 'reactivePower':
+            return 'mVAR';
+        case 'speed':
+            return 'm/s';
+        case 'temperature':
+            return 'C';
+        case 'time':
+            return 'ms';
+        case 'voltage':
+            return 'mV';
+        case 'volume':
+            return 'ml';
+        case 'volumeFlowRate':
+            return 'ml/s';
+        default:
+            return convert().describe(unit).abbr;
+        }
     }
 }
--- src/models/v1/ProductQuantity.ts
+++ src/models/v1/ProductQuantity.ts
@@ -1,6 +1,72 @@
+import convert, { Unit } from 'convert-units';
+import DatabaseManager from '../../database/DatabaseManager';
+import Utils from '../../Utils';
+
+const quantityCache: Map<string, DatabaseQuantity> = new Map();
+
 export interface ProductQuantity {
     value: number;
-    unit: string;
+    unit: Unit;
     amount: number;
     total: number;
+}
+
+export type DatabaseQuantity = ProductQuantity & { id: number };
+
+function hashQuantity(quantity: ProductQuantity): string {
+    return quantity.value + quantity.unit + quantity.amount;
+}
+
+export async function findQuantity(
+    database: DatabaseManager,
+    quantity: ProductQuantity,
+): Promise<DatabaseQuantity> {
+    // Normalize quantity to default form
+    const newUnit = Utils.getSmallestMatchingUnit(quantity.unit);
+    const newValue = convert(quantity.value).from(quantity.unit).to(newUnit);
+
+    const normalizedQuantity = {
+        value: newValue,
+        unit: newUnit,
+        amount: quantity.amount,
+        total: newValue * quantity.amount,
+    };
+    // Check if we already know the id for this quantity
+    const hashedQuantity: string = hashQuantity(normalizedQuantity);
+
+    if (quantityCache.has(hashedQuantity)) {
+        return quantityCache.get(hashedQuantity) as DatabaseQuantity;
+    }
+
+    // Check if the quantity is already in the database
+    const foundResults = await database.query(
+        'SELECT id FROM quantity WHERE quantity_value = ?, quantity_amount = ?, quantity_unit = ?;',
+        [normalizedQuantity.value, normalizedQuantity.amount, normalizedQuantity.unit],
+    );
+    // The quantity already exists
+    if (foundResults.length > 0) {
+        quantityCache.set(hashedQuantity, {
+            id: foundResults[0] as number,
+            ...normalizedQuantity,
+        });
+    } else {
+        // Create a new quantity
+        const createdResults = await database.query(
+            'INSERT INTO quantity (quantity_value, quantity_amount, quantity_unit) VALUES (?, ?, ?) RETURNING id;',
+            [normalizedQuantity.value, normalizedQuantity.amount, normalizedQuantity.unit],
+        );
+
+        if (createdResults.length > 0) {
+            quantityCache.set(hashedQuantity, {
+                id: createdResults[0] as number,
+                ...normalizedQuantity,
+            });
+        }
+    }
+
+    if (quantityCache.has(hashedQuantity)) {
+        return quantityCache.get(hashedQuantity) as DatabaseQuantity;
+    }
+
+    throw new Error('Was not able to find or create quantity');
 }
